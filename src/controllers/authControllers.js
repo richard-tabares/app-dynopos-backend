32de6a3781b0226d38ff9aa67656136bda931aa5
Richard Tabares <richardtabaresb@gmail.com>
2026-04-14 18:48:48 -0500

    Update: autentificación

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,50 +1,66 @@
 import { supabase } from '../config/supabase.js'
 
 export const login = async (req, res) => {
-	const { user, pass } = req.body
-	try {
-		const { data, error } = await supabase.auth.signInWithPassword({
-			email: user,
-			password: pass,
-		})
-		if (error) throw new Error(error)
-		return res.json({ status: 200, message: 'Login exitoso', data })
-	} catch (error) {
-		return res.status(500).json({ error: error.message })
-	}
+    const { email, password } = req.body
+    try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+        const { data: profileData, error: profileError } = await supabase
+            .from('profiles')
+            .select('*')
+            .eq('id', data.user.id)
+			.single()
+        if (profileError) throw new Error(profileError)
+        if (error) throw new Error(error)
+        return res.json({
+            status: 200,
+            message: 'Login exitoso',
+            data,
+            profile: profileData,
+        })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 export const signup = async (req, res) => {
-	const { user, pass } = req.body
+    const { email, password } = req.body
 
-	try {
-		const { data, error } = await supabase.auth.signUp({
-			email: user,
-			password: pass,
-		})
+    try {
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        })
 		await supabase
 			.from('profiles')
-			.insert({ id: data.user.id, role: 'admin' })
-		if (error) throw new Error(error)
-		return res.json({
-			status: 201,
-			message: 'Usuario creado exitosamente',
-			data,
-		})
-	} catch (error) {
-		return res.status(500).json({ error: error.message })
-	}
+			.insert({
+            id: data.user.id,
+            display_name: 'Richard Tabares',
+            role: 'admin',
+            business_id: 1,
+        })
+        if (error) throw new Error(error)
+        return res.json({
+            status: 201,
+            message: 'Usuario creado exitosamente',
+            data,
+        })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 export const logout = async (req, res) => {
-	try {
-		const { error } = await supabase.auth.signOut()
-		if (error) throw new Error(error)
-		return res.json({
-			status: 200,
-			message: 'Se ha cerrado sesión exitosamente',
-		})
-	} catch (error) {
-		return res.status(500).json({ error: error.message })
-	}
+    try {
+        const { error } = await supabase.auth.signOut()
+        if (error) throw new Error(error)
+        return res.json({
+            status: 200,
+            message: 'Se ha cerrado sesión exitosamente',
+        })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
