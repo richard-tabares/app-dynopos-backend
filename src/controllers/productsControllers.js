7b45fc6892e3ed5578010c6384823d7222009e02
Richard Tabares <richardtabaresb@gmail.com>
2026-04-22 16:42:23 -0500

    Se adiciona una endopoint nuevo getProductById

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,51 +1,84 @@
-import { supabase } from "../config/supabase.js";
+import { supabase } from '../config/supabase.js'
 
 export const getProducts = async (req, res) => {
-
     const { data, error } = await supabase
         .from('products')
-        .select('*')
-    
-    if(error) return res.status(500).json(error)
-    res.json(data)
+        .select(
+            `id,
+            name,
+            sku,
+            price,
+            is_active,
+            categories (
+                id,
+                name
+            )`
+        )
+        .eq('business_id', req.params.businessId)
 
+    if (error) return res.status(500).json(error)
+    res.json(data)
+}
+export const getProductById = async (req, res) => {
+    const { data, error } = await supabase
+        .from('products')
+        .select(
+            `id,
+            name,
+            sku,
+            price,
+            is_active,
+            categories (
+                id,
+                name
+            )`
+        )
+        .eq('id', req.params.ProductId)
+        .single()
+
+    if (error) return res.status(500).json(error)
+    res.json(data)
 }
 
 export const createProduct = async (req, res) => {
-   
     const { data, error } = await supabase
         .from('products')
         .insert(req.body)
-        .select()
-    
-    res.status(201).json({ status: 201, message: 'Producto Creado', data })
-    
+        .select(
+            `id,
+            name,
+            sku,
+            price,
+            is_active,
+            categories (
+                id,
+                name
+            )`
+        )
+
     if (error) return res.status(500).json(error)
-    
+    res.status(201).json({ status: 201, message: 'Producto Creado', data })
 }
 export const updateProduct = async (req, res) => {
-
-    const { id } = req.params
+    const { ProductId } = req.params
     const { data, error } = await supabase
         .from('products')
         .update(req.body)
-        .eq('id', id)
+        .eq('id', ProductId)
         .select()
-    
+
     if (error) return res.status(500).json(error)
     res.json({ status: 200, message: 'Producto Actualizado', data })
-    
 }
 export const deleteProduct = async (req, res) => {
-    const { id } = req.params
+    const { ProductId } = req.params
 
     const { data, error } = await supabase
         .from('products')
         .delete()
-        .eq('id', id)
+        .eq('id', ProductId)
         .select()
-    
+
     if (error) return res.status(500).json(error)
     res.json({ status: 200, message: 'Producto Eliminado', data })
-    
 }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { getProducts, createProduct, updateProduct, deleteProduct } from '../controllers/productsControllers.js'
+import { getProducts, createProduct, updateProduct, deleteProduct, getProductById } from '../controllers/productsControllers.js'
 
 const router = Router()
 
-router.get('/', getProducts)
-router.post('/', createProduct)
-router.patch('/:id', updateProduct)
-router.delete('/:id', deleteProduct)
+router.get('/:businessId', getProducts)
+router.get('/product/:ProductId', getProductById)
+router.post('/createProduct', createProduct)
+router.patch('/:ProductId', updateProduct)
+router.delete('/:ProductId', deleteProduct)
 
 export default router
